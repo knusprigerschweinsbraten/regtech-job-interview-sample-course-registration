857325533d38c8c58742241a635b4268178ff4f4
spachler <stefan.pachler@bearingpoint.com>
2021-07-08 20:47:12 +0200

    test is green now. yippee ki yay!

--- src/main/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImpl.java
+++ src/main/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImpl.java
@@ -1,6 +1,12 @@
 package knusprigerschweinsbraten.regtech.jobinterview;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * @author juergen.windhaber
@@ -10,6 +16,25 @@ import java.util.List;
 class CourseRegistrationEvaluatorImpl implements CourseRegistrationEvaluator {
     @Override
     public List<String> findAllParticipants(List<CourseRegistration> registrations) {
-        throw new UnsupportedOperationException("Please implement me!");
+        final Map<String, CourseRegistration> latestRegistrationsPerName = registrations.stream()
+                .collect(toMap(CourseRegistration::getCourseParticipant, Function.identity(), new TakeLatestRegistrationMergeFunction()));
+        return latestRegistrationsPerName.entrySet().stream()
+                .filter(entry -> entry.getValue().isEnrolled())
+                .map(Map.Entry::getKey)
+                .collect(toList());
+    }
+
+    /**
+     * @author stefan.pachler
+     * @since 0.0.1
+     */
+    private static class TakeLatestRegistrationMergeFunction implements BinaryOperator<CourseRegistration> {
+        @Override
+        public CourseRegistration apply(CourseRegistration oldOne, CourseRegistration newOne) {
+            if (oldOne.getRegistrationDate().isBefore(newOne.getRegistrationDate())) {
+                return newOne;
+            }
+            return oldOne;
+        }
     }
 }
