fa33229e832639405c92a30c2200a0d4dfd0287e
spachler <stefan.pachler@bearingpoint.com>
2021-07-08 20:44:58 +0200

    test is green now. yippee ki yay!

--- src/main/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImpl.java
+++ src/main/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImpl.java
@@ -1,6 +1,13 @@
 package knusprigerschweinsbraten.regtech.jobinterview;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Function;
+
+import static java.util.Comparator.comparing;
+import static java.util.function.BinaryOperator.maxBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * @author juergen.windhaber
@@ -10,6 +17,16 @@ import java.util.List;
 class CourseRegistrationEvaluatorImpl implements CourseRegistrationEvaluator {
     @Override
     public List<String> findAllParticipants(List<CourseRegistration> registrations) {
-        throw new UnsupportedOperationException("Please implement me!");
+        // first of all go through all available registrations and take the latest one of each person
+        final Collection<CourseRegistration> latestRegistrations = registrations.stream()
+                .collect(toMap(CourseRegistration::getCourseParticipant, Function.identity(), maxBy(comparing(CourseRegistration::getRegistrationDate))))
+                .values();
+
+        // once we have all the latest registration entries we need to filter based on the status. So
+        // everyone who is enrolled will be counted.
+        return latestRegistrations.stream()
+                .filter(CourseRegistration::isEnrolled)
+                .map(CourseRegistration::getCourseParticipant)
+                .collect(toList());
     }
 }
