7dfb3e05029141b6c0d0995bf3a2306081bf5c88
martin.kuerbisch <martin.kuerbisch@regnology.net>
2022-10-24 13:16:12 +0200

    Adapt some namings and upgrade versions

--- src/test/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImplTest.java
+++ src/test/java/knusprigerschweinsbraten/regtech/jobinterview/CourseRegistrationEvaluatorImplTest.java
@@ -1,12 +1,13 @@
 package knusprigerschweinsbraten.regtech.jobinterview;
 
 import knusprigerschweinsbraten.regtech.jobinterview.CourseRegistration.Disenrollment;
-import knusprigerschweinsbraten.regtech.jobinterview.CourseRegistration.Enrolment;
+import knusprigerschweinsbraten.regtech.jobinterview.CourseRegistration.Enrollment;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CourseRegistrationEvaluatorImplTest {
@@ -15,14 +16,15 @@ class CourseRegistrationEvaluatorImplTest {
     @Test
     void tryingToFindAllParticipants() {
         final List<CourseRegistration> registrations = Arrays.asList(
-                new Enrolment("Stefan", "2018-01-01"),
-                new Enrolment("Isabella", "2018-02-03"),
-                new Enrolment("Martin", "2017-01-02"),
+                new Enrollment("Stefan", "2018-01-01"),
+                new Enrollment("Isabella", "2018-02-03"),
+                new Enrollment("Martin", "2017-01-02"),
                 new Disenrollment("Stefan", "2016-12-13"),
                 new Disenrollment("Isabella", "2018-02-04"));
 
         final List<String> result = classUnderTest.findAllParticipants(registrations);
 
+        assertEquals(result.size(), 2);
         assertTrue(result.containsAll(Arrays.asList("Martin", "Stefan")));
     }
 }
